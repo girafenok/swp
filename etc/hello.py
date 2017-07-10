77bfd2924323e2f2590fcabb48ad208c0e10dce3
girafenok <denisbk@gmail.com>
2017-07-10 18:32:17 +0000

    5 commit

--- etc/hello.py
+++ etc/hello.py
@@ -1,3 +1,3 @@
 def hello_app(environ, start_response):
 	start_response('200 OK', [('Content-type', 'text/plain')])
-    return "\n".join(environ.get('QUERY_STRING').split("&"))
+	return "\n".join(environ.get('QUERY_STRING').split("&"))
---
+++ git.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git config --global user.email "denisbk@gmail.com"
+git config --global user.name "girafenok"
